681d3bd301e6900d3acc96afee19e079a75ecdef
yfsteven <yaninquiry@gmail.com>
2024-02-28 17:51:58 -0500

    Added link to /new on index page

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Messageboard", messages: messages })
+  res.render('index', { title: "Mini Message Board", messages: messages })
 });
 
 router.get('/new', function(req, res, next) {
